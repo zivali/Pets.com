2f40b06f2bbf90dc7e0e36d95df4cb87b5ab2d51
zivalee <zivalee87@gmail.com>
2019-08-16 01:02:09 +0800

    fix card image arrangement

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,42 +1,48 @@
   <template>
-      <b-container>
-        <div>
-          <b-row>
-            <div v-bind:key="data.index" v-for="data in pets">
-              <b-col l="4">
-                <b-card class="col-3 text-center " 
-                  v-bind:title="data.animal_kind"
-                  img v-if="data.album_file" :img-src="data.album_file" alt="Card image cap"
-                  img-top
-                  tag="article"
-                  style="max-width: 20rem;"
-                  >
-                  <!--<b-card-text>{{ `${data.strCategoryDescription.slice(0,100)}...` }}</b-card-text>-->
-                  <b-button href="#" variant="info">View pet</b-button>
-                </b-card>
-              </b-col>
-            </div>
-          </b-row>
+  <b-container>
+    <div>
+      <b-card-group deck class="mt-3">
+        <!--所有card都在同一個-->
+        <div v-bind:key="data.index" v-for="data in pets">
+          <b-card
+            class="col-auto text-center mb-3 card-img-top img-fluid"
+
+            v-bind:title="data.animal_kind"
+            img
+            v-if="data.album_file"
+            :img-src="data.album_file"
+            alt="Image"
+            img-top
+            tag="article"
+            style="width: 20rem;"
+          >
+            <!--<b-card-text>{{ `${data.strCategoryDescription.slice(0,100)}...` }}</b-card-text>-->
+            <b-button href="#" variant="info">View pet</b-button>
+          </b-card>
         </div>
-      </b-container>
-    </template>
+      </b-card-group>
+    </div>
+  </b-container>
+</template>
     
     <script>
-    export default {
-      data() {
-        return {
-          pets: []
-        };
-      },
-     mounted() {
-        fetch('https://cors-anywhere.herokuapp.com/http://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL')
-            .then(response => response.json())
-            .then((data) => {
-                this.pets = data;
-            })
-        },
+export default {
+  data() {
+    return {
+      pets: []
     };
-    </script>
+  },
+  mounted() {
+    fetch(
+      "https://cors-anywhere.herokuapp.com/http://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL"
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.pets = data;
+      });
+  }
+};
+</script>
     <style>
-    @import './img.css';
-    </style>
+@import "./img.css";
+</style>
